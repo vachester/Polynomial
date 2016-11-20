6185e3297ca76bc3a2b3a7a3ba88829bbf817bad
vachester <xcchester@gmail.com>
2016-11-20 23:53:10 +0800

    Lab6

---
+++ src/PolynomialTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PolynomialTest {
+	Polynomial test = new Polynomial();
+	@Before
+	public void setUp() throws Exception {
+		test.expression("x^2*y");
+		test.restore();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDerivative() {
+		test.derivative("!d/dz");
+		test.combine();
+		test.show();
+		String result = test.toString();
+		assertEquals("0", result);
+	}
+
+}
